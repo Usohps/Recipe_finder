70c2f3aac987bf3d67c9a5ac560e86cfedf21d20
Princewill Usoh <princewillusoh@Princewills-MacBook-Pro.local>
2026-01-31 00:51:55 +0100

    Fix/Cron.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { neon } from "@neondatabase/serverless";
+import job from "./config/cron.js";
 
 const sql = neon(ENV.DATABASE_URL);
 const app = express();
 const PORT = ENV.PORT;
+if (ENV.NODE_ENV === "production") job.start();
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true, message: "API is healthy" });
